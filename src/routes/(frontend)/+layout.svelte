51f4e3496cb69fdcefae4a7a002d9952bd012f4b
Mitchell Christ <mitchell@human.marketing>
2025-04-17 09:24:38 -0700

    add `@vercel/analytics`

--- src/routes/(frontend)/+layout.svelte
+++ src/routes/(frontend)/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment'
 	import Header from '@/ui/header/Header.svelte'
 	import Footer from '@/ui/footer/Footer.svelte'
 	import '@/styles/app.css'
 
 	let { children } = $props()
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' })
 </script>
 
 <Header />
